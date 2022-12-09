20533ad36e54420c1e8dc3553fcc4d40c47301d1
Brayan Sosa <101683480+BrayanSosa04@users.noreply.github.com>
2022-12-09 08:59:11 -0600

    Update display.py
    This is the display function used in our project to help us display images. It can be considered a helper function for the "main" file in this folder

--- Functions/display.py
+++ Functions/display.py
@@ -6,3 +6,4 @@ from phantominator import shepp_logan
 def display(img): #this is a function to display images, it takes in the matrix(image) that I want to display
     plt.imshow(img, cmap='gray')
     plt.show() #creates the little pop-up window
+    
